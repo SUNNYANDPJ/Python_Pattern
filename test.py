cc6892234cbb036fcb53e6cef9e03826f740f5a5
SUNNYGAME <1183679527@qq.com>
2015-08-14 09:42:12 +0800

    create package manage&SimpleFactory

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 #coding:utf-8
+import sys
+sys.path.append('Singleton')
+sys.path.append('Factory')
 from Singleton_1 import Singleton as single
 from Singleton_1_1 import Singleton as single1
 from Singleton_2 import Singleton as single2
+from SimpleFactory import SimpleFactory as sf
 
 @single2
 class S2(object):
@@ -31,6 +35,11 @@ def main():
 	s2 = S2.Instance()
 	print id(s1),s1.display()
 	print id(s2),s2.display()
+
+	import random
+	t = random.choice(['bc','pc'])
+	course = sf.create_course(t)
+	print course.get_labs()
 
 
 if __name__ == '__main__':
